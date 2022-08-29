ad4a0bde80ae19be68c07b374643ab4514549fde
laijiefeng <laijiefeng@mail.btc38.com>
2022-08-29 18:00:18 +0800

    新增创建表 sql 文件

---
+++ create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `t_article` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `create_time` char(19) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `type` tinyint(1) NOT NULL DEFAULT '1' COMMENT '类型 1.原创 2.摘抄',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `t_article_detail` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `article_id` int NOT NULL DEFAULT '0',
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `create_time` char(19) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `article_id` (`article_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
