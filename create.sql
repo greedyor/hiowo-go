32bbada1124ff7d11e3b904f6f7516e6e474ab5a
赖杰丰 <44224558+greedyor@users.noreply.github.com>
2022-08-29 22:51:45 +0800

    Update create.sql
    调整表结构前缀

--- create.sql
+++ create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `t_article` (
+CREATE TABLE `ob_article` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
   `create_time` char(19) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
@@ -6,7 +6,7 @@ CREATE TABLE `t_article` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `t_article_detail` (
+CREATE TABLE `ob_article_detail` (
   `id` int NOT NULL AUTO_INCREMENT,
   `article_id` int NOT NULL DEFAULT '0',
   `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE `t_article_detail` (
   UNIQUE KEY `article_id` (`article_id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `t_visits` (
+CREATE TABLE `ob_visits` (
   `id` int NOT NULL AUTO_INCREMENT,
   `ip` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
   `type` tinyint(1) NOT NULL DEFAULT '1',
