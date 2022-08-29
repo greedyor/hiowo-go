6320ee9c7f7bcd9233969bbff0e60b968bdf32d2
laijiefeng <laijiefeng@mail.btc38.com>
2022-08-29 18:03:58 +0800

    新增访问记录表

--- create.sql
+++ create.sql
@@ -14,3 +14,11 @@ CREATE TABLE `t_article_detail` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `article_id` (`article_id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `t_visits` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ip` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `type` tinyint(1) NOT NULL DEFAULT '1',
+  `visit_time` char(19) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3066 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
